8c2098164ed0a229401cf8a14a2e3d955a8b4560
jackyr1 <103156038+jackyr1@users.noreply.github.com>
2024-05-07 22:50:29 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -2,17 +2,14 @@
 // Author: Jacky Ramos <jramos54@ucsc.edu>
 // Date: 7 May
 
-
-
-// Using jQuery to select the element by its ID and set the HTML content
-$("#outputt").html('Original array: ')
-
-
+$("#output").html("Look what it can do!")
 
 // add button to challenge, problems, and reflection section
 $("#challenge").append("<button class= 'buttonC' id='button-challenge'>Make Special</button>");
 $("#problems").append("<button class= 'buttonC' id='button-problems'>Make Special</button>");
 $("#reflection").append("<button class= 'buttonC' id='button-reflection'>Make Special</button>");
+$("#results").append("<button class= 'buttonC' id='button-results'>Make Special</button>");
+
 
 // add a click listener to the challenge button
 $("#button-challenge").click(function(){
@@ -31,6 +28,13 @@ $("#button-problems").click(function(){
 $("#button-reflection").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#reflection").toggleClass("special");
+});
+
+
+// add a click listener to the reflection button
+$("#button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
 });
 
 
